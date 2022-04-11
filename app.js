3ff5c92a2889479cb80d0c36ba497f7e3d43b96f
nambi13 <69418124+nambi13@users.noreply.github.com>
2022-04-11 16:39:08 +0300

    asdasdas

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const express=require('express');
+// Install express server 
+const express = require('express');
 const path = require('path');
-const app=express();
-
-app.use(express.static('./dist/m1p9mean-frontangular-Mickael'));
-
-
-app.get('/*',(req,res)=>
-    res.sendFile('index.html',{root:'dist/angular-heroku'}),
-);
-
-app.listen(process.env.PORT || 3000)
+const app = express();
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/front'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/front/index.html'));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
