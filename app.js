391bf98f84d67cd07674edee250d139707920996
nambi13 <69418124+nambi13@users.noreply.github.com>
2022-04-11 16:21:33 +0300

    debut deploy

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express=require('express');
 const path = require('path');
 const app=express();
 
-app.use(express.static('./dist/front'));
+app.use(express.static('./dist/m1p9mean-frontangular-Mickael'));
 
 
 app.get('/*',(req,res)=>
-    res.sendFile('index.html',{root:'dist/angular/heroku'}),
+    res.sendFile('index.html',{root:'dist/angular-heroku'}),
 );
 
 app.listen(process.env.PORT || 3000)
